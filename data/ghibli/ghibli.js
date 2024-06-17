6c0bf5cd2cfc1b8ffe5f2fc3d9243495e93ba946
Ro LV <138071814+RocioLV@users.noreply.github.com>
2024-06-17 17:26:16 -0400

    Update ghibli.js
    Se soluciona el problema de carga de algunas imágenes

--- data/ghibli/ghibli.js
+++ data/ghibli/ghibli.js
@@ -7,7 +7,7 @@ export default {
       "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
       "director": "Hayao Miyazaki",
       "producer": "Isao Takahata",
-      "poster": "https://ghiblicollection.com/cdn/shop/products/CITS_Cover_SB_72dpi.png?v=1675274073",
+      "poster": "https://ghiblicollection.com/cdn/shop/products/CITS.DVD.Cover.72dpi.jpg?v=1675274187",
       "release_date": "1986",
       "rt_score": "95",
       "people": [
@@ -878,7 +878,7 @@ export default {
       "description": "Shizuku lives a simple life, dominated by her love for stories and writing. One day she notices that all the library books she has have been previously checked out by the same person: 'Seiji Amasawa'. Curious as to who he is, Shizuku meets a boy her age whom she finds infuriating, but discovers to her shock that he is her 'Prince of Books'. As she grows closer to him, she realises that he merely read all those books to bring himself closer to her. The boy Seiji aspires to be a violin maker in Italy, and it is his dreams that make Shizuku realise that she has no clear path for her life. Knowing that her strength lies in writing, she tests her talents by writing a story about Baron, a cat statuette belonging to Seiji's grandfather.",
       "director": "Yoshifumi Kondō",
       "producer": "Toshio Suzuki",
-      "poster": "https://ghiblicollection.com/cdn/shop/products/WOTF_Cover_SB_72dpi.png?v=1675276186",
+      "poster": "https://ghiblicollection.com/cdn/shop/products/Whisper.DVD.Cover.72dpi.jpg?v=1713199755",
       "release_date": "1995",
       "rt_score": "91",
       "people": [
@@ -1339,7 +1339,7 @@ export default {
       "description": "Haru, a schoolgirl bored by her ordinary routine, saves the life of an unusual cat and suddenly her world is transformed beyond anything she ever imagined. The Cat King rewards her good deed with a flurry of presents, including a very shocking proposal of marriage to his son! Haru embarks on an unexpected journey to the Kingdom of Cats where her eyes are opened to a whole other world.",
       "director": "Hiroyuki Morita",
       "producer": "Toshio Suzuki",
-      "poster": "https://ghiblicollection.com/cdn/shop/products/CatReturns_Cover_SteelBook_72dpi.png?v=1675796408",
+      "poster": "https://ghiblicollection.com/cdn/shop/products/CatReturns.DVD.Cover.72dpi.jpg?v=1675796386",
       "release_date": "2002",
       "rt_score": "89",
       "people": [
